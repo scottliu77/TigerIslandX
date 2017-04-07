6a41537390907b2d297a316ecd2a4ad2140176e4
colinbnd <cbandish@ufl.edu>
2017-04-07 07:17:32 -0400

    This might be a little bit silly

---
+++ src/JUnit/GameManagerTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import java.awt.*;
+
+public class GameManagerTest {
+    @Test
+    public void gameManagerTest() throws Exception{
+        GameManager testGameManager = new GameManager(true);
+        Point center = new Point(492,364);
+        HexButton hexButton = testGameManager.getBoard().getHexButton(center);
+        assert(hexButton.getHex().getTypeName().equals("VOLCANO"));
+    }
+    @Test
+    public void GUITest() throws Exception{
+        TigerIsland tigerIsland = new TigerIsland();
+        tigerIsland.createAndShowGUI();
+    }
+}
